c5d20e06d10a827060d795cde797edc2204a8832
ronen <blixa@Lias-MacBook-Air.local>
2025-07-14 16:17:55 -0700

    Improve day box layout: limit to 2 columns max for better description writing

--- components/WeeklyScheduler.tsx
+++ components/WeeklyScheduler.tsx
@@ -337,7 +337,7 @@ const WeeklyScheduler = ({ onSignOut }: WeeklySchedulerProps) => {
           </div>
         )}
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {weekDates.map((date, dayIndex) => {
             const dayEntries = scheduleData[dayIndex] || []
             
